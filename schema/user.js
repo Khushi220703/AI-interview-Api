3051de737479f9a640abcd81f24b6330c2e60eb7
khushi22072003 <kj365268@gmail.com>
2026-06-08 23:37:52 +0530

    fixed schema name

--- schema/user.js
+++ schema/user.js
@@ -143,6 +143,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("interviewUser", userSchema);
 
 export default User;
